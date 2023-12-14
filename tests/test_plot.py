387e37de979874c4c29594372d6ae506377da237
Timothee Mathieu <timothee.mathieu@inria.fr>
2023-12-14 10:35:50 +0100

    test plot sota

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -20,3 +20,18 @@ def test_plot():
             evals = {"Agent "+str(k): np.random.normal(size=n) for k in range(n_agents)}
         comparator.partial_compare(evals)
     comparator.plot_results()
+
+    
+def test_plot_sota():
+    n_agents = 3
+    comparisons = np.array([(0,i) for i in [1,2]])
+    comparator = MultipleAgentsComparator(n=n, K=K, B=B,  alpha=alpha, comparisons=comparisons, seed=42, beta = 0.01)
+    evals = {}
+    while not comparator.is_finished:
+        if len(evals) >0:
+            for k in range(n_agents):
+                evals["Agent "+str(k)] = np.hstack([evals["Agent "+str(k)] ,np.random.normal(size=n)])
+        else:
+            evals = {"Agent "+str(k): np.random.normal(size=n) for k in range(n_agents)}
+        comparator.partial_compare(evals)
+    comparator.plot_results_sota()
