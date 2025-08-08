78903f347364edee3619bc7a2a77d579a0132ca1
TimotheeMathieu <timothee.mathieu@inria.fr>
2025-08-08 11:24:20 +0200

    tests sota

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -70,7 +70,7 @@ def test_plot_sota_noteq():
             for k in range(n_agents):
                 evals["Agent "+str(k)] = np.hstack([evals["Agent "+str(k)] ,np.random.normal(size=10)+k])
         else:
-            evals = {"Agent "+str(k): np.random.normal(size=n) for k in range(n_agents)}
+            evals = {"Agent "+str(k): np.random.normal(size=10) for k in range(n_agents)}
         comparator.partial_compare(evals)
     comparator.plot_results_sota()
     # plt.savefig('fig2.pdf')
