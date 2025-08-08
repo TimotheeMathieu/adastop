bdfe12de66c7a2edf153c1ab9f3d4aacc0b7ce76
TimotheeMathieu <timothee.mathieu@inria.fr>
2025-08-08 11:24:01 +0200

    tests sota

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -58,3 +58,21 @@ def test_plot_noteq():
     # plt.savefig('fig2.pdf')
     fig, axes= plt.subplots(1,2)
     comparator.plot_results(axes=axes)
+
+def test_plot_sota_noteq():
+    n_agents = 3
+    comparisons = np.array([(0,i) for i in [1,2]])
+    comparator = MultipleAgentsComparator(n=10, K=K, B=B,  alpha=alpha, 
+                                          comparisons=comparisons, seed=42, beta = 0.01)
+    evals = {}
+    while not comparator.is_finished:
+        if len(evals) >0:
+            for k in range(n_agents):
+                evals["Agent "+str(k)] = np.hstack([evals["Agent "+str(k)] ,np.random.normal(size=10)+k])
+        else:
+            evals = {"Agent "+str(k): np.random.normal(size=n) for k in range(n_agents)}
+        comparator.partial_compare(evals)
+    comparator.plot_results_sota()
+    # plt.savefig('fig2.pdf')
+    fig, axes= plt.subplots(1,2)
+    comparator.plot_results_sota(axes=axes)
