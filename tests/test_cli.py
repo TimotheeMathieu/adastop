02eb3d1512cef69816fbd279c605802383f574dc
JulienT01 <julien.tj@gmail.com>
2025-08-11 15:16:38 +0200

    add tests on missing comparator file

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 import pytest
 from click.testing import CliRunner
 from adastop.cli import adastop
+import os
 
 # we reuse a bit of pytest's own testing machinery, this should eventually come
 import subprocess
@@ -8,6 +9,11 @@ import subprocess
 
 def test_cli():
     runner = CliRunner()
+    test_pdf_path = "test.pdf"
+
+    if os.path.exists(test_pdf_path):
+        os.remove(test_pdf_path)
+
     result = runner.invoke(adastop, ['reset', 'examples'])
     assert result.exit_code == 0
     for j in range(1,6):
@@ -18,10 +24,12 @@ def test_cli():
     result = runner.invoke(adastop, ['compare', 'examples/walker3.csv'])
     assert result.exit_code == 1
 
-    result = runner.invoke(adastop, ['plot', 'examples', "test.pdf"])
+    result = runner.invoke(adastop, ['plot', 'examples', test_pdf_path])
     assert result.exit_code == 0
     result = runner.invoke(adastop, ['status', 'examples'])
     assert result.exit_code == 0
+    assert os.path.exists(test_pdf_path) == True
+
 
     result = runner.invoke(adastop, ['reset', 'examples'])
     assert result.exit_code == 0
